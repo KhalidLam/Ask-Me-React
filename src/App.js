368989e32de69b5b1ca9b3c2c75c8d3b239f50af
Gklam <khalid.lamsadi@outlook.fr>
2020-08-29 16:55:23 +0100

    Add HomePage, Header, SideBar, RightSideBar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
+import HomePage from "./pages/HomePage/HomePage";
+
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+
+      <Header />
+      {/* <Alert /> */}
+      <Switch>
+          <Route exact path='/' component={HomePage} />
+          {/* <Route exact path='/questions' component={QuestionsPage} />
+          <Route exact path='/tags' component={TagsPage} />
+          <Route exact path='/users' component={UsersPage} />
+          <Route exact path='/jobs' component={HomePage} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/questions/:id' component={Post} />
+          <Route exact path='/users/:id' component={UserPage} />
+          <Route exact path='/tags/:tagname' component={TagPage} />
+          <Route exact path='/add/question' component={PostForm} />*/}
+        </Switch>
     </div>
   );
 }
